40fd1c995b60d5dd9ea529d2bc72ab6b05be1acb
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 06:42:11 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SceneKitDSL",
+    products: [
+        .library(
+            name: "SceneKitDSL",
+            targets: ["SceneKitDSL"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SceneKitDSL",
+            dependencies: [],
+            path: "SceneKitDSL")
+    ]
+)
